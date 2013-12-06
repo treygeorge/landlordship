c71a986ddb2706e7c419866a4ac779a3de44486c
treygeorge <trey@Treys-MacBook-Pro.local>
2013-12-06 15:04:38 -0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Landlordship::Application.config.secret_key_base = 'ebf7a366fe06b0dd8c3b2e396efc0d6404784a6511c6037d2c9ff1818ad0ffa89897b0b2bd0ffcdf8476135f239fcf534f71b441e3db78b23fa8fcf82b4654e0'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Landlordship::Application.config.session_store :cookie_store, key: '_landlordship_session'
